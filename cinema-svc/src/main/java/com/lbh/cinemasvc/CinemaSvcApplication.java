0a1f86e2c5e12d9ac95256054e831d0d6504f586
Li Banghao <banghao.li@mathartsys.com>
2021-04-23 15:56:38 +0800

    initialize multi-modules project. Done DP-0

---
+++ cinema-svc/src/main/java/com/lbh/cinemasvc/CinemaSvcApplication.java
@@ -0,0 +1,13 @@
+package com.lbh.cinemasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSvcApplication.class, args);
+    }
+
+}
