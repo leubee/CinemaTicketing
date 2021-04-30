c6f53eaae7b6c41f03948ae75bb3f6524b5a9521
Li Banghao <banghao.li@mathartsys.com>
2021-04-30 16:31:12 +0800

    Daily Work

---
+++ cinema-ui/src/components/AsyncProgrammingTemplate.vue
@@ -0,0 +1,45 @@
+<template>
+  <br/>
+</template>
+
+<script>
+export default {
+  name: "AsyncProgrammingTemplate",
+  created() {
+    this.addSecInOdd()
+    this.addSecInEven()
+  },
+  data() {
+    return {
+      count : 0,
+    }
+  },
+  methods: {
+    async addSecInEven() {
+      await setTimeout(()=>null, 1000)
+      while (true) {
+        await setTimeout(this.addCount, 1000)
+      }
+    },
+
+    async addSecInOdd() {
+      while (true) {
+        await setTimeout(this.addCount, 1000)
+      }
+    },
+
+    addCount() {
+      this.count += 1
+      console.log('Count is now '+ this.count)
+    }
+
+  }
+}
+
+export class name {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ cinema-ui/src/components/ElementUIComponentSet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul type="none">
+    <li>
+      <el-button type="success">BUTTON-SUCCESS</el-button>
+      <el-button type="warning">BUTTON-WARNING</el-button>
+      <el-button type="danger">BUTTON-DANGER</el-button>
+    </li>
+    <li>
+      <el-table>
+        <el-table-column>
+
+        </el-table-column>
+        <el-table-column>
+
+        </el-table-column>
+      </el-table>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: "ElementUIComponentSet",
+  created() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
